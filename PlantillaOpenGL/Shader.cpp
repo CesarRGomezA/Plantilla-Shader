0c30df5d5570bb501d8167dcc4cbd4b57f65529d
Instalacion de Softawres <software@ulsa-noroeste.edu.mx>
2019-02-27 09:48:01 -0700

    Completamos  4 pasos

--- PlantillaOpenGL/Shader.cpp
+++ PlantillaOpenGL/Shader.cpp
@@ -24,4 +24,51 @@ Shader::Shader(const char * rutaVertex, const char * rutaFragment)
 	else {
 		cout << "No se pudo abrir el archivo" << rutaVertex;
 	}
+
+	string codigoFragmentShader;
+	ifstream fragmentShaderStream(
+		rutaFragment, ios::in
+	);
+	if (fragmentShaderStream.is_open()) {
+		string linea;
+		while (getline(fragmentShaderStream, linea))
+		{
+			codigoFragmentShader += linea + "\n";
+		}
+
+		fragmentShaderStream.close();
+	}
+	else
+	{
+		cout << "No se pudo abrir el archivo" 
+		<< rutaVertex << endl;
+	}
+
+	//Convertir de string a cadena de char
+	const char* cadenaCodigoVertex = codigoVertexShader.c_str();
+	const char* cadenaCodigoFragment = codigoFragmentShader.c_str();
+	
+	//1.Crear el programa de Shader
+	shaderID = glCreateProgram();
+	GLuint vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+
+	GLuint fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+
+	//2.Cargar el codigo del shader 
+	glShaderSource(vertexShaderID, 1, &cadenaCodigoVertex, NULL);
+	glShaderSource(fragmentShaderID, 1, &cadenaCodigoFragment, NULL);
+
+	//3.Compilar los shaders 
+	glCompileShader(vertexShaderID);
+	glCompileShader(fragmentShaderID);
+
+	//4. Verificar errores de compilación
+
+}
+
+void Shader::verificarCompilacion(GLuint id) 
+{
+	GLint resultado = GL_FALSE;
+	int longitydlog = 0;
 }
